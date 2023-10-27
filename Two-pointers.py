d920fe315a35d835b37cc4cc76f956d2c4d3a8cc
Zakaria Coulibaly <kategnimincoulibaly@MacBook-Pro.local>
2023-10-27 08:14:17 -0400

    Two pointers pattern

---
+++ Two-pointers.py
@@ -0,0 +1,69 @@
+"""
+Whenever there’s a requirement to find two data elements in an array that
+satisfy a certain condition, the two pointers pattern should be the first
+strategy to come to mind.
+# Using two pointers can make the solution faster than using a nested loop,
+form N^2 to O(N)
+
+Yes,    if all of these conditions are fulfilled:
+    The input data can be traversed in a linear fashion,
+    that is, it’s in an array,
+    in a linked list, or in a string of characters.
+
+    We limit our focus to a specific range of elements within
+     the input data, as dictated
+    by the positions of the two pointers, allowing us to consider
+     a small subset of elements
+    rather than the entire set.
+
+
+No, if either of these conditions is fulfilled:
+    The input data cannot be traversed in a linear fashion, that is, it’s neither in an array, nor in
+    a linked list, nor in a string of characters.
+    The problem requires an exhaustive search of the solution space, that is, eliminating one solution
+    does not eliminate any others.
+"""
+
+'''
+Problem 1:
+
+Note: A palindrome is a word, phrase, or sequence of characters that reads the same backward as forward.
+
+# Simplify version
+def is_palindrome(s: str) -> bool:
+    left, right = 0, len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left, right = left + 1, right - 1
+    return True
+'''
+
+
+def is_palindrome(s: str) -> bool:
+    # Remove any non-alphanumeric characters and convert to lowercase
+    clean_s = ''.join(ch for ch in s if ch.isalnum()).lower()
+
+    # Initialize two pointers at the beginning and end of the string
+    left, right = 0, len(clean_s) - 1
+
+    # Continue comparing characters while the left pointer is less than the right pointer
+    while left < right:
+        if clean_s[left] != clean_s[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+
+# Test the function
+string = "A man, a plan, a canal, Panama!"
+print(is_palindrome(string))  # True
+
+s = "abba"
+s2 = "kayak"
+s3 = "malayalam"
+s4 = "levis"
+
+print(is_palindrome(s4))
